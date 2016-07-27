ba2a45059d32d375a83902f186630f83ec80d9af
Sergio Emir Baidon Carrillo <SergioBaidon@MacBook-Pro-de-Sergio.local>
2016-07-27 14:27:26 -0600

    fixes

--- devServer.js
+++ devServer.js
@@ -13,7 +13,7 @@ app.use(require('webpack-dev-middleware')(compiler, {
 
 
 
-app.use('/static', express.static('public'));
+app.use('/static', express.static(__dirname + 'public'));
 
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));

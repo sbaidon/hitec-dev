d8adbabcdbdd7ed50b5a81a5a7278e71e1730000
Sergio Emir Baidon Carrillo <SergioBaidon@MacBook-Pro-de-Sergio.local>
2016-07-27 14:41:53 -0600

    fix webpack

--- devServer.js
+++ devServer.js
@@ -1,21 +1,12 @@
 var path = require('path');
 var express = require('express');
-var webpack = require('webpack');
-var config = require('./webpack.config');
 
 var app = express();
-var compiler = webpack(config);
-
-app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath
-}));
-
 
 
 app.use('/static', express.static(__dirname + 'dist'));
 
-app.get('/', function(req, res) {
+app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
